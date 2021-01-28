bff42eaa8017df18f07c3663f5e2c82f753d2664
Sandeep Kumar <sandeep@DESKTOP-MC78NCI.localdomain>
2021-01-28 17:02:37 +0530

    Add files

--- Day 2/read.c
+++ Day 2/read.c
@@ -5,6 +5,8 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+#include<pthread.h>
+#include<semaphore.h>
 
 int main(int argc, char *argv[])
 {
@@ -12,40 +14,26 @@ int main(int argc, char *argv[])
     char buff[128];
     fd = open("in.txt", O_RDONLY);
     int c = fork();
-
-// to read line by line
-    // while (1)
-    // {
-    //     char buf1[1];
-    //     if(c > 0){
-    //         printf("Parent Word: ");
-    //         while(read(fd, buff, 1) != ' '){
-    //             printf("%s", buff);
-    //         }
-    //     } else
-    //     {
-    //         printf("Parent Word: ");
-    //         while(read(fd, buff, 1) != ' '){
-    //             printf("%s", buff);
-    //         }
-    //     }
-    //     if(read(fd, buff, 1) == 0) break; 
-    // }
-    
-
+    sem_t parent, child;
+    sem_init(&parent, 1, 1);
+    sem_init(&child, 1, 0);
 
 // read 10 in parent and 10 char in child
-   while( read(fd, buff, 10) > 0) {
+   while( read(fd, buff, 11) > 0) {
     //    read(fd, buff, 10);
        if(c > 0){
-            // printf("Parent printing\n");
+            printf("Parent printing\n");
+            sem_wait(&parent);
             printf("Parent: %s\n", buff);
+            sem_post(&child);
         } else
         {
-            // printf("child printing\n");
-            printf("child: %s\n", buff);   
+            printf("child printing\n");
+            sem_wait(&child);
+            printf("child: %s\n", buff);  
+            sem_post(&parent); 
         }
    }
    
   return 0;
-}
+}   
